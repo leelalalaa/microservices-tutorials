e9128d2f7e27c48947f5d1dacdf8bafee124688e
leelalalaa <leelapinnamaraju@gmail.com>
2025-01-26 12:10:54 -0500

    initial setup with dependencies and all

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+//const express = require('express'); archaic way of doing this
+import express from 'express'; 
+
+const app = express(); 
+
+app.get("/", (req,res) => {
+    res.send("Server is ready");
+});
+
+app.listen(5000, () => {
+    console.log("Server started at http://localhost:5000 hello"); 
+});
