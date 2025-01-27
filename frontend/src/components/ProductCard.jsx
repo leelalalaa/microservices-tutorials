fe5eb314587c9f9211cd088dd495d1e7e62b5f92
leelalalaa <leelapinnamaraju@gmail.com>
2025-01-27 17:55:57 -0500

    not gonna do update - my brain is fried

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,7 +1,23 @@
 import { useProductStore } from '../store/product'; 
+import { useState } from "react"; 
+import {
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalFooter,
+	ModalHeader,
+	ModalOverlay,
+    useDisclosure,
+} from "@chakra-ui/react";
 
 const ProductCard = ({product}) => {
-    const { deleteProduct } = useProductStore(); 
+    const [updatedProduct, setUpdatedProduct] = useState(product);
+
+    const { deleteProduct, updateProduct  } = useProductStore(); 
+    
+    const { isOpen, onClose } = useDisclosure();
+
     const handleDeleteProduct = async (pid) => {
         const {success,message} = await deleteProduct(pid); 
         if(!success) {
@@ -10,13 +26,40 @@ const ProductCard = ({product}) => {
             console.log("it worked");
         }
     }
+
+    const handleUpdateProduct = async (pid, updatedProduct) => {
+		const { success, message } = await updateProduct(pid, updatedProduct);
+		onClose();
+		if(!success) {
+            console.log("it didn't work");
+        } else {
+            console.log("it worked");
+        }
+	};
     
     return ( 
         <div>
             <p>{product.name}</p>
             <p>{product.price}</p>
             <p>{product.image}</p>
-            <button>edit</button>
+            <Modal isOpen={isOpen} onClose={onClose}>
+				<ModalOverlay />
+
+				<ModalContent>
+					<ModalHeader>Update Product</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>
+                        <input type="text" placeholder="Product Name" value={updatedProduct.name} name="name" onChange={(e) => setUpdatedProduct({ ...updatedProduct, name: e.target.value })}/>
+                        <input type="text" placeholder="Price" value={updatedProduct.price} name="price" onChange={(e) => setUpdatedProduct({ ...updatedProduct, price: e.target.value })}/>
+                        <input type="text" placeholder="Image" value={updatedProduct.image} name="image" onChange={(e) => setUpdatedProduct({ ...updatedProduct, image: e.target.value })}/>
+					</ModalBody>
+
+					<ModalFooter>
+                        <button onClick={() => handleUpdateProduct(product._id)}>Update</button>
+						<button onClick={onClose}>cancel</button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
             <button onClick={() => handleDeleteProduct(product._id)}>delete</button>
         </div>
     );
