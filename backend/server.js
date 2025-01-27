277576a2ee6af5dbece7520407bd43f708da7ab8
leelalalaa <leelapinnamaraju@gmail.com>
2025-01-27 00:13:51 -0500

    got the delete endpoint working for entries that have been alr deleted

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,15 @@ app.post("/api/products", async (req,res) => {
 app.delete("/api/products/:id", async (req,res) => {
     const {id} = req.params; 
     console.log("id:", id);
+    try {
+        const deletedProduct = await Product.findByIdAndDelete(id); 
+        if(!deletedProduct) {
+            return res.status(404).json({ success: false, message: "Product not found" }); 
+        }
+        res.status(200).json({ success: true, message: "Product Deleted" });
+    } catch (error) {
+        res.status(404).json({ success: false, message: "Product not found" }); 
+    }
 });
 
 app.listen(5000, () => {
