53b01ef1a2b9089457cec06a856bdd64a32d038e
leelalalaa <leelapinnamaraju@gmail.com>
2025-01-27 12:00:50 -0500

    stinky code (terrible front end) but it's functional

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import CreatePage from "./pages/CreatePage"; 
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <>
+      <Link to="/">Home</Link> {/* Link to HomePage */}   
+      <Link to="/create">Create Page</Link> {/* Link to CreatePage */}
+          
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
 const CreatePage = () => {
-    return (
-        <div>CreatePage</div>
-    )
-}
+    const [newProduct, setNewProduct] = useState({
+        name: "", 
+        price: "",
+        image: "", 
+    });
+
+    const handleAddProduct = () => {
+        console.log(newProduct);
+    }
+
+    return <div>
+        <p>Create New Product</p>
+        <input type="text" placeholder="Product Name" value={newProduct.name} name="name" onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}/>
+        <input type="text" placeholder="Price" value={newProduct.price} name="price" onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}/>
+        <input type="text" placeholder="Image" value={newProduct.image} name="image" onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}/>
+        <input type="submit" value="Submit" onClick={handleAddProduct} />
+    </div>;
+};
 
 export default CreatePage; 
